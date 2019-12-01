47d51940eaa246385d9379a303f3364247cf1754
Ygor Barboza Alves <50305486+YgorAlves@users.noreply.github.com>
2019-12-01 04:36:22 -0300

    Update sagas.js

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -17,7 +17,7 @@ export function* updateProfile({ payload }) {
 
     const response = yield call(api.pu, 'users', profile);
 
-    toast.sucess('Perfil atualizado com sucesso!');
+    toast.success('Perfil atualizado com sucesso!');
 
     yield put(updateProfileSuccess(response.data));
   } catch (err) {
